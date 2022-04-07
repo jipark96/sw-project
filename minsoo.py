5304ab783e1db732317f7c9b10750613f5a43304
minsoo <als494@naver.com>
2022-04-07 14:51:32 +0900

    강의고유번호, 경희대 교수면 수정 함수

--- minsoo.py
+++ minsoo.py
@@ -1,16 +1,17 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_excel("C:/Users/minsoo/Desktop/22년 1학기 강의목록 원본/경희대학교 국제캠퍼스22년 1학기 강의목록.xlsx")
+df = pd.read_excel("./input/경희대학교 국제캠퍼스22년 1학기 강의목록.xlsx")
 
-#경희대 강의고유번호
-df[['강의고유번호', '지울 부분']] = df['강좌코드강좌코드'].str.split('-', n=1, expand=True)     # '-' 기준으로 나누기
-df = df.drop(['강좌코드강좌코드', '지울 부분'], axis=1)     # 필요없는 열 삭제
+# 강의고유번호 수정 함수
+# 모든 대학이 '-' 뒤 삭제라서 다 똑같음
+def lectureNumber():
+    df['강의고유번호'] = df['강의고유번호'].str.split('-').str.get(0)     # '-' 기준으로 나누고 첫번째만 추출
+    return df
 
-
-#경희대 교수명
-df['교수명'] = df['교수명교수명'].str.replace(pat=' / ..', repl='', regex=False)    # 교수명 수정
-df['교수명'] = df['교수명'].str.replace(pat='..', repl='', regex=False)
-df['교수명'] = df['교수명'].str.replace(pat=' / ', repl=',', regex=False)
-
-df = df.drop(['교수명교수명'], axis=1)      # 필요없는 열 삭제
+#경희대 교수명 수정 함수
+def professorName():
+    df['교수명'] = df['교수명'].str.replace(pat=' / ..', repl='', regex=False)    # 교수명 수정
+    df['교수명'] = df['교수명'].str.replace(pat='..', repl='', regex=False)
+    df['교수명'] = df['교수명'].str.replace(pat=' / ', repl=',', regex=False)
+    return df
