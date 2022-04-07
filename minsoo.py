791e3ab97f5f10d36459ed5ecbd2bafa09bfded5
minsoo <als494@naver.com>
2022-04-07 17:11:05 +0900

    고려대 강의명, 한국과학기술원 교수명 수정함수

--- minsoo.py
+++ minsoo.py
@@ -9,9 +9,21 @@ def lectureNumber():
     df['강의고유번호'] = df['강의고유번호'].str.split('-').str.get(0)     # '-' 기준으로 나누고 첫번째만 추출
     return df
 
-#경희대 교수명 수정 함수
+# 경희대 교수명 수정 함수
 def professorName():
     df['교수명'] = df['교수명'].str.replace(pat=' / ..', repl='', regex=False)    # 교수명 수정
     df['교수명'] = df['교수명'].str.replace(pat='..', repl='', regex=False)
     df['교수명'] = df['교수명'].str.replace(pat=' / ', repl=',', regex=False)
+    return df
+
+# 고려대 강의명 수정 함수
+def lectureName():
+    df['강의명'] = df['강의명'].str.split('(').str.get(0)     # '(' 기준으로 나누고 첫번째만 추출
+    return df
+
+# 한국과학기술원 교수명 수정 함수
+def professorName():
+    df['교수명'] = df['교수명'].str.replace(pat=' 외', repl='', regex=False)    # 교수명 수정
+    df['교수명'] = df['교수명'].str.replace(pat=' ,', repl=',', regex=False)
+    df['교수명'] = df['교수명'].str.replace(pat='담당교수미정', repl='', regex=False)
     return df
