3893701082f87de630a9590c5f4015e5945feac7
jihyun <wlgus996@naver.com>
2022-04-09 15:14:57 +0900

    연세대 교수명 여러개 분리

--- jihyun1.py
+++ jihyun1.py
@@ -4,11 +4,10 @@ import pandas as pd
 #엑셀 불러오기
 df = pd.read_excel
 
-#연세대학교 교수명
-def professorName():
-    df = df['교수명'].str.split(',')  #split 각 행을 여러 컬럼으로 나눔
-    df=df.apply(lambda x: pd.Series(x))  #배열이 Series를 리턴하게 apply 적용, DataFrame 변환
-    df=df.stack().reset_index(level=1, drop=True).to_frame('교수명') #stack 컬럼을 행으로 변환, 인덱스 초기화
+#연세대학교 교수명 여러개 분리 함수
+def professorYS(df):
+    df['교수명'] = df['교수명'].str.split(',')
+    df = df.explode('교수명')
     return df
 
 
